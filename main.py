80cbd9722cb96baa4aff9c08dc24d208a9b5289d
AlbinaKhominiatych <hominyatichalbina@gmail.com>
2023-11-20 20:26:38 +0200

    property()

--- main.py
+++ main.py
@@ -1,20 +1,30 @@
-#функтори
-#метод __call__
-import time
-class Timer:
-    def __init__(self, func):
-        self.func = func
+#property()
+class Circle:
+    def __init__(self, radius):
+        self._radius = radius
 
-    def __call__(self, *args, **kwargs):
-        start = time.time()
-        print(f"Викликаємо функцію {self.func.__name__}")
-        result = self.func(*args, **kwargs)
-        end = time.time()
-        print(f"Функція виконувалася {end - start:.5f}" )
-        return result
+    def get_radius(self):
+        print("get_radius")
+        return self._radius
 
-@Timer
-def summa():
-    return sum(range(1, 100000))
+    def set_radius(self, value):
+        print("set_radius")
+        if value < 0:
+            raise ValueError("Значення радіусу не може бути меньше 0")
+        self._radius = value
 
-print(summa())
+    def delete_radius(self):
+        print("delete_radius")
+        del self._radius
+
+    radius = property(get_radius, set_radius, delete_radius)
+
+circle = Circle(10)
+#print(circle.get_radius())
+#circle.set_radius(100)
+#print(circle.get_radius())
+print(circle.radius) #fget=get_radius
+circle.radius = 100
+print(circle.radius)
+del circle.radius
+#print(circle.radius)
