ac7756694d9d81d2497262784f3b4eca9c704cea
AlbinaKhominiatych <hominyatichalbina@gmail.com>
2023-11-20 20:42:31 +0200

    @property зі статичними атрибутами

--- main.py
+++ main.py
@@ -1,34 +1,19 @@
-#property()
-class Circle:
-    def __init__(self, radius):
-        self._radius = radius
+class MyClass:
+    _x = None
 
     @property
-    def radius(self):
-        print("get_radius")
-        return self._radius
+    def x(self):
+        return self._x
 
-    @radius.setter
-    def radius(self, value):
-        print("set_radius")
+    @x.setter
+    def x(self, value):
         if value < 0:
-            raise ValueError("Значення радіусу не може бути меньше 0")
-        self._radius = value
+            self._x = 0
+        else:
+            self._x = value
 
-    @radius.deleter
-    def radius(self):
-        print("delete_radius")
-        del self._radius
-
-    #radius = property(get_radius, set_radius, delete_radius)
-    #radius = radius.getter(get_radius)
-
-circle = Circle(10)
-#print(circle.get_radius())
-#circle.set_radius(100)
-#print(circle.get_radius())
-print(circle.radius) #fget=get_radius
-radius = 100
-print(circle.radius)
-del circle.radius
-print(circle.radius)
+obj = MyClass()
+obj.x = 10  # Виведе: 10
+print(obj.x)
+obj.x = -5  # Виведе: 0
+print(obj.x)
