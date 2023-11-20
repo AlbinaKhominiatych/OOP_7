9788431f73f83fb143c8f0e556fa72bd6f4c99da
AlbinaKhominiatych <hominyatichalbina@gmail.com>
2023-11-20 20:39:11 +0200

    @property

--- main.py
+++ main.py
@@ -3,28 +3,32 @@ class Circle:
     def __init__(self, radius):
         self._radius = radius
 
-    def get_radius(self):
+    @property
+    def radius(self):
         print("get_radius")
         return self._radius
 
-    def set_radius(self, value):
+    @radius.setter
+    def radius(self, value):
         print("set_radius")
         if value < 0:
             raise ValueError("Значення радіусу не може бути меньше 0")
         self._radius = value
 
-    def delete_radius(self):
+    @radius.deleter
+    def radius(self):
         print("delete_radius")
         del self._radius
 
-    radius = property(get_radius, set_radius, delete_radius)
+    #radius = property(get_radius, set_radius, delete_radius)
+    #radius = radius.getter(get_radius)
 
 circle = Circle(10)
 #print(circle.get_radius())
 #circle.set_radius(100)
 #print(circle.get_radius())
 print(circle.radius) #fget=get_radius
-circle.radius = 100
+radius = 100
 print(circle.radius)
 del circle.radius
-#print(circle.radius)
+print(circle.radius)
