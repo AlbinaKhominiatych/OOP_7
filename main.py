4c442a0b0576b9ea4f86a8b5832bd8a0ed595c7a
AlbinaKhominiatych <hominyatichalbina@gmail.com>
2023-11-20 20:45:53 +0200

    властивості

--- main.py
+++ main.py
@@ -1,19 +1,34 @@
-class MyClass:
-    _x = None
+#property()
+class Circle:
+    def __init__(self, radius):
+        self._radius = radius
 
     @property
-    def x(self):
-        return self._x
+    def radius(self):
+        print("get_radius")
+        return self._radius
 
-    @x.setter
-    def x(self, value):
+    @radius.setter
+    def radius(self, value):
+        print("set_radius")
         if value < 0:
-            self._x = 0
-        else:
-            self._x = value
+            raise ValueError("Значення радіусу не може бути меньше 0")
+        self._radius = value
 
-obj = MyClass()
-obj.x = 10  # Виведе: 10
-print(obj.x)
-obj.x = -5  # Виведе: 0
-print(obj.x)
+    @radius.deleter
+    def radius(self):
+        print("delete_radius")
+        del self._radius
+
+    #radius = property(get_radius, set_radius, delete_radius)
+    #radius = radius.getter(get_radius)
+
+circle = Circle(10)
+#print(circle.get_radius())
+#circle.set_radius(100)
+#print(circle.get_radius())
+print(circle.radius) #fget=get_radius
+radius = 100
+print(circle.radius)
+del circle.radius
+print(circle.radius)
